8f6b90b1d3e25b9a776484c86ba1ab1df2b9324e
JJRomero <josejromero24@gmail.com>
2023-11-21 17:08:39 +0100

    Create base project.

---
+++ Movie_Catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_Catalog
+//
+//  Created by José Javier Romero on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
